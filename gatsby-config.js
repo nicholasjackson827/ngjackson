896628553c6bbb0ac993bde6eb291c004132edc3
Nicholas Jackson <ngjackson16@transy.edu>
2018-03-02 20:06:33 -0500

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Nicholas Jackson\'s Blog',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    }
+  ],
+};
